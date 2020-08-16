e745d51c4c7b8f281e8bdf57ea4f71e94b2192c2
linuxchr <69758634+linuxchr@users.noreply.github.com>
2020-08-16 16:56:56 +0200

    Add files via upload

---
+++ cin.sh
@@ -0,0 +1,24 @@
+echo Make Cinnamon Programm Version 1.0 Beta
+echo Es gibt keine Haftung für Probleme
+read -p "Enter zum fortfahrenn
+clear
+echo [##----------------------------] 2%
+sleep 1.0
+clear
+sudo apt-get install cinnamon unzip git -y
+sudo apt-get install p7zip -y
+clear
+echo 100 % <#############################>
+sleep 1.0
+clear
+echo desing-sh kann installiert werden
+echo Enter zum Fortfahren
+echo Kreuz zum schließen.
+read
+cd /tmp
+git clone https://github.com/linuxchr/mad.git
+cd mad
+unzip the.zip
+chmod u+x the.sh
+./the.sh
+reboot
